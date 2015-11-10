e832c175ddcb9866036a933d2077152dabd3de3c
WadaHayato <hatoya.web.design@gmail.com>
2015-11-10 16:13:32 +0900

    load angular

--- front/js/application.js
+++ front/js/application.js
@@ -1,4 +1,24 @@
 (function() {
+
+    var app = angular.module('balloonApp', ['ngRoute', 'ngResource']);
+
+    app.config(function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/template/index.html',
+                controller: 'topPageController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
+
+
     $(window).scroll(function() {
         if($(window).scrollTop() >= 500) {
             $('#js-header').addClass('on');
