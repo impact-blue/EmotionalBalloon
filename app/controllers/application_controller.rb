e095802b584b3d3c77de68e30223ae699ca95533
西村健人 <negai.m.nishimura@gmail.com>
2015-11-26 14:17:14 +0900

    add company controller、seed file(city)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  #商品詳細ページ、発送ページ、利用規約、下層
+
 end
