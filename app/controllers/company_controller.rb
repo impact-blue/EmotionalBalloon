55048649dc6eb17f0d9aa6adc3be1fb8c6a5e0ce
西村健人 <negai.m.nishimura@gmail.com>
2015-11-26 14:17:14 +0900

    add company controller、seed file(city)

---
+++ app/controllers/company_controller.rb
@@ -0,0 +1,24 @@
+class CompanyController < ApplicationController
+
+  def agreement
+  end
+
+  def privacy
+  end
+
+  def exchange
+  end
+
+  def question
+  end
+
+  def about
+  end
+
+  def flow
+  end
+
+  def contact
+  end
+
+end
