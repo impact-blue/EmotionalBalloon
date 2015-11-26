10b6589eaf3066a684e25235fda76fe3b465273c
西村健人 <negai.m.nishimura@gmail.com>
2015-11-26 14:26:13 +0900

    companyのviewとルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,15 @@ Rails.application.routes.draw do
 
   get 'api/show' => 'api#show'
 
+
+  get 'company/agreement' => 'company#agreement'
+  get 'company/privacy' =>'company#privacy'
+  get 'company/exchange' => 'company#exchange'
+  get 'company/question' => 'company#question'
+  get 'company/about' => 'company#about'
+  get 'company/flow' => 'company#flow'
+  get 'company/contact' => 'company#contact'
+
+
 #mount API => "/api"
 end
