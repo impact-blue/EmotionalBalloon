c4f539fcd8b7a97dabb56075018175e81c25f979
西村健人 <negai.m.nishimura@gmail.com>
2015-11-26 11:42:26 +0900

    add model user

---
+++ db/migrate/20151126022443_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.string :email
+      t.integer :phone
+      t.integer :postal_code
+      t.integer :city_id
+      t.string :address
+      t.string :address2
+
+      t.timestamps null: false
+    end
+  end
+end
