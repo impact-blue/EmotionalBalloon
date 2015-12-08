9a94075502bdfc329fb045ec451fdbaec3320c3b
西村健人 <negai.m.nishimura@gmail.com>
2015-12-08 18:38:55 +0900

    注文情報、顧客情報のview,controller追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
-  belongs_to :products
+  belongs_to :product
   belongs_to :payment #未
-  belongs_to :user
+  belongs_to :user, foreign_key: 'user_id'
   belongs_to :city
   belongs_to :scene
 end
