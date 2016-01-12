2efe6f544811fb409f478d5b34cc7fc86cf4627d
西村健人 <negai.m.nishimura@gmail.com>
2016-01-12 18:22:47 +0900

    add capistranioとnginxの設定

--- config/deploy/deploy.rb
+++ config/deploy/deploy.rb
@@ -4,9 +4,23 @@ set :application, 'emotional_balloon'
 set :repo_url, 'git@github.com:impact-blue/EmotionalBalloon.git'
 
 set :deploy_to, 'var/www/app/EmotionalBalloon'
+set :pty,true
 set :scm, :git
 
 set :keep_releases,5
+set :user,"balloon"
+set :password,"impact1234"
 
-namespace :deploy do |variable|
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:app), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
 end
