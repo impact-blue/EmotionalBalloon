7c070e310c773311f243d246e8d3397f39f12985
西村健人 <negai.m.nishimura@gmail.com>
2016-01-13 11:31:07 +0900

    fix cap,unicorn

--- config/deploy/deploy.rb
+++ config/deploy/deploy.rb
@@ -2,25 +2,17 @@ lock '3.4.0'
 
 set :application, 'emotional_balloon'
 set :repo_url, 'git@github.com:impact-blue/EmotionalBalloon.git'
-
 set :deploy_to, 'var/www/app/EmotionalBalloon'
+
 set :pty,true
 set :scm, :git
 
 set :keep_releases,5
 set :user,"balloon"
+set :use_sudo, true
 set :password,"impact1234"
 
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:app), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+  after "deploy", "deploy:restart"
 end
