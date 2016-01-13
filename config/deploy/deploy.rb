8f6398aa7bc2005e912e8abaa505160d1c204168
西村健人 <negai.m.nishimura@gmail.com>
2016-01-13 11:45:35 +0900

    fix namespace

--- config/deploy/deploy.rb
+++ config/deploy/deploy.rb
@@ -13,6 +13,25 @@ set :use_sudo, true
 set :password,"impact1234"
 
 
+
 namespace :deploy do
-  after "deploy", "deploy:restart"
+  Rake::Task["deploy:updating"].clear_actions
+  task :updating => :new_release_path do
+    #invoke "#{scm}:create_release" gitを使わないので不要
+    # 代わりに、wgetでMavenリポジトリからjar/warファイルをダウンロードするようにする
+    on release_roles :all do
+      execute :mkdir, '-p', release_path
+      execute :wget, "#{fetch(:m2repo)}/#{fetch(:group)}/#{fetch(:application)}/#{fetch(:version)}/#{fetch(:application)}-#{fetch(:version)}.war -P #{release_path}"
+    end
+
+    #invoke "deploy:set_current_revision" gitを使わないので不要
+    # 代わりに、入力させたバージョンをREVISIONファイルに書き込む
+    on release_roles :all do
+      within release_path do
+        execute :echo, "\"#{fetch(:version)}\" >> REVISION"
+      end
+    end
+
+    invoke 'deploy:symlink:shared'
+  end
 end
