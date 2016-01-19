7dc04a23b3d86f23d0bcb325197627d3447be042
西村健人 <negai.m.nishimura@gmail.com>
2016-01-19 13:13:27 +0900

    add 管理画面の商品項目のコントローラーを再定義（作り直し）

--- test/controllers/admin_products_controller_test.rb
+++ test/controllers/admin_products_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AdminsControllerTest < ActionController::TestCase
+class AdminProductsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
