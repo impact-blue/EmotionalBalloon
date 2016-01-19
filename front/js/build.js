03d2524d33549b93553f51d602d4143b8a0047a3
WadaHayato <hatoya.web.design@gmail.com>
2016-01-19 16:38:14 +0900

    fix path

--- front/js/build.js
+++ front/js/build.js
@@ -54,16 +54,16 @@ app.config(["$routeProvider", "$locationProvider", "$httpProvider", function($ro
             templateUrl: '/template/admin/index.html',
             controller: 'adminPageController'
         })
-        .when('/admin/product', {
+        .when('/company/privacy', {
+            templateUrl: '/template/company/privacy.html'
+        })
+        .when('/admin/products', {
             templateUrl: '/template/admin/product/index.html',
             controller: 'adminPageController'
         })
-        .when('/admin/order', {
+        .when('/admin/orders', {
             templateUrl: '/template/admin/order/index.html',
             controller: 'adminPageController'
-        })
-        .when('/company/privacy', {
-            templateUrl: '/template/company/privacy.html'
         })
         .otherwise({
             redirectTo: '/'
