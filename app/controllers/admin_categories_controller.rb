7dcc98d96d45ae44568771d37b2de3e5c7a73e5c
西村健人 <negai.m.nishimura@gmail.com>
2016-01-19 18:35:05 +0900

    fix admin_categories　コントローラー

--- app/controllers/admin_categories_controller.rb
+++ app/controllers/admin_categories_controller.rb
@@ -1,6 +1,44 @@
 class AdminCategoriesController < ApplicationController
+  before_action :logged_in_admin_user
 
   def index
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(create_params)
+
+    if @category.save
+      redirect_to 'index'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(create_params)
+      flash[:success] = "編集しました"
+      redirect_to admin_products_path, notice: '商品の編集が完了しました。'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:category).permit()
+  end
+
+  def logged_in_admin_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to admin_login_path
+    end
   end
 
 end
