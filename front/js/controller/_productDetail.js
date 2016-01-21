5d492b2b4c2e1f5bfbb3001e08d4ab015f413d86
WadaHayato <hatoya.web.design@gmail.com>
2016-01-21 17:02:33 +0900

    add save to cart

--- front/js/controller/_productDetail.js
+++ front/js/controller/_productDetail.js
@@ -1,8 +1,30 @@
 app.controller('productDetailController', function($scope, $http) {
+    $scope.is_cart = false;
+    $scope.cartItem = cartItem;
     $http({
         method: 'GET',
-        url: '/apis/products/detail.json'
+        url: '/api/products/detail.json'
     }).success(function(data, status, headers, config) {
         $scope.data = data.data;
+        angular.forEach($scope.cartItem, function(value, key){
+            if($scope.data.id === value) {
+                $scope.is_cart = true;
+            }
+        });
     });
+
+    $scope.toCart = function(id, flag) {
+        var tmpItem = $scope.cartItem;
+        $scope.cartItem = [];
+        $scope.is_cart = !$scope.is_cart;
+        angular.forEach(tmpItem, function(value, key){
+            if(id !== value) {
+                $scope.cartItem.push(value);
+            }
+        });
+        if(flag) {
+            $scope.cartItem.push(id);
+        }
+        localStorage.setItem("cart", JSON.stringify($scope.cartItem));
+    };
 });
