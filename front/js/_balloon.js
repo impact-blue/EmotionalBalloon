e1d70210f14692074f3abeca9e56cbb82f5bc4d2
WadaHayato <hatoya.web.design@gmail.com>
2016-01-26 18:49:02 +0900

    add angular-bootstrap-datetimepicker

--- front/js/_balloon.js
+++ front/js/_balloon.js
@@ -1,8 +1,9 @@
 require('angular');
 require('angular-resource');
 require('angular-route');
+require('angular-bootstrap-datetimepicker');
 
-var app = angular.module('balloonApp', ['ngRoute', 'ngResource']),
+var app = angular.module('balloonApp', ['ngRoute', 'ngResource', 'ui.bootstrap.datetimepicker']),
     cartItem = [];
 
 if(localStorage.getItem('cart')) {
