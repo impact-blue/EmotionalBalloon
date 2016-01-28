08860b0a97f1b3eb7c5d3042130cec17e6c0ef1d
WadaHayato <hatoya.web.design@gmail.com>
2016-01-28 16:02:38 +0900

    gulp build

---
+++ public/js/controller/_cartComfirm.js
@@ -0,0 +1,15 @@
+app.controller('cartComfirmController', function($scope, $http) {
+    $scope.test = {
+        sample1: 'HelloWorld!!',
+        sample2: 'hogehoge'
+    };
+    $scope.cartComfirm = function() {
+        $http({
+            method: 'POST',
+            url: '/api/carts/comfirm',
+            data: $scope.test
+        }).success(function(data, status, headers, config) {
+            console.log(data, status);
+        });
+    };
+});
