c2fe60a82cbbe14499834e1133665b57aa0ea1c7
西村健人 <negai.m.nishimura@gmail.com>
2016-01-29 14:47:24 +0900

    fix カートのapi

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,32 +8,26 @@ class CartsController < ApplicationController
   end
 
   def comfirm
-    @token = "true"
+  end
+
+  def api
+    @product = Cart.new(cart_params)
+    success = {data:{result: "success"}}
+    if @product.save
+     # redirect_to carts_thanks_path
+      render json: success
+    else
+      redirect_to carts_thanks_path
+    end
   end
 
   def thanks
   end
 
-  def create_cart
-    @cart = Cart.new(cart_params)
-    remote_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
-    @cart.ip = set_ip
-    @cart.flag = 0
-    if @cart.save
-      redirect_to :back
-    else
-      render 'show'
-    end
-  end
-
   private
 
   def cart_params
-    params.require(:cart).permit(:product_id,:ip)
-  end
-
-  def set_ip
-    @ip = request.remote_ip
+    params.require(:cart).permit(:product_id)
   end
 
 
