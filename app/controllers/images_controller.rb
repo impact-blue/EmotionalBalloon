bc19fb391d80d7d1d87fb8acb4ef7f91bc38215e
西村健人 <negai.m.nishimura@gmail.com>
2016-01-29 18:10:47 +0900

    add　画像の保存と表示

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,37 @@
+class ImagesController < ApplicationController
+
+  def index
+    @images = Image.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @images }
+    end
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(create_params)
+    @image.image =params[:image][:image].read
+    @image.image_content_type = params[:image][:image].content_type
+
+    if @image.save
+      redirect_to action: 'index'
+    else
+      render new
+    end
+  end
+
+  def get_image
+    @image = Image.find(params[:id])
+    send_data(@image.image, :disposition => "inline", :type => "image/png")
+  end
+
+  private
+  def create_params
+    params.require(:image).permit(:name)
+  end
+
+end
