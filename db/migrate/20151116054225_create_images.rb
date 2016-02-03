07e6623cb5c8123f042ca3403b19795a6b463323
西村健人 <negai.m.nishimura@gmail.com>
2016-02-03 12:59:48 +0900

    fix imageのモデルを調整

--- db/migrate/20151116054225_create_images.rb
+++ db/migrate/20151116054225_create_images.rb
@@ -1,6 +1,7 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
+      t.integer :product_id
       t.string :name
       t.binary :image, limit: 10.megabyte
       t.string :image_content_type
