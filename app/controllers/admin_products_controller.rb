b5bae40a0ce16810bca94216f3ae77a0347ff95e
西村健人 <negai.m.nishimura@gmail.com>
2016-02-04 11:12:00 +0900

    fix json

--- app/controllers/admin_products_controller.rb
+++ app/controllers/admin_products_controller.rb
@@ -1,5 +1,6 @@
 class AdminProductsController < ApplicationController
   before_action :logged_in_admin_user
+  before_action :json_index
 
   def new
     @product = Product.new
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < ActionController::Base
 
 
 
+
   private
     def basic
       authenticate_or_request_with_http_basic do |user, pass|
@@ -28,6 +29,50 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       flash[:danger] = "ログインしてください"
       redirect_to admin_login_path
+    end
+  end
+
+
+
+  def json_index
+    min = params[:min]
+    max = params[:max]
+    price = params[:price]
+    scene = params[:scene]
+    chara = params[:chara]
+    color = params[:color]
+    balloon_type = params[:balloon_type]
+    @page = 3
+
+    if min.present? && max.present?
+      @json_products = Product.where('price >= ? AND price <= ?',params[:min],params[:max]).page(params[:page]).per(@page).order("created_at DESC")
+      @current_page = params[:page].to_i
+
+#単一検索
+    elsif min.present? #最小価格
+      @json_products = Product.where('price >= ? ',params[:min]).page(params[:page]).per(@page).order("created_at DESC")
+      @current_page = params[:page].to_i
+    elsif max.present? #最大価格
+      @json_products = Product.where('price <= ? ',params[:max]).page(params[:page]).per(@page).order("created_at DESC")
+      @current_page = params[:page].to_i
+    elsif scene.present? #シーン別
+      @json_products = Kaminari.paginate_array(Product.includes(:scenes).where('scene = ?', params[:scene]).references(:scene).order("products.created_at DESC")).page(params[:page]).per(@page)
+      @current_page = params[:page].to_i
+
+    elsif chara.present? #キャラクター別
+      @json_products = Kaminari.paginate_array(Product.includes(:charas).where('chara = ?', params[:chara]).references(:chara).order("products.created_at DESC")).page(params[:page]).per(@page)
+      @current_page = params[:page].to_i
+
+    elsif color.present? #色別
+      @json_products = Kaminari.paginate_array(Product.includes(:colors).where('color = ?', params[:color]).references(:color).order("products.created_at DESC")).page(params[:page]).per(@page)
+      @current_page = params[:page].to_i
+    elsif balloon_type.present? #バルーンタイプ別
+      @json_products = Kaminari.paginate_array(Product.includes(:balloon_types).where('balloon_type = ?', params[:balloon_type]).references(:balloon_type).order("products.created_at DESC")).page(params[:page]).per(@page)
+      @current_page = params[:page].to_i
+#条件なし
+    else
+      @json_products = Product.page(params[:page]).per(@page).order("created_at DESC")
+      @current_page = params[:page].to_i
     end
   end
 
