0878f0c79f4b3489d172fed8bb145c7af77a8987
WadaHayato <hatoya.web.design@gmail.com>
2016-02-04 11:08:27 +0900

    fix table style

--- front/js/controller/_admin.js
+++ front/js/controller/_admin.js
@@ -1,11 +1,5 @@
 app.controller('adminController', function($scope, $http) {
     $scope.path = location.pathname;
     $scope.search = location.search;
-
-    $http({
-        method: 'GET',
-        url: apiUrl[1]
-    }).success(function(data, status, headers, config) {
-        $scope.data = data.data;
-    });
+    $scope.data = balloon_data.data;
 });
