53a63a5d039fe0cc32e295b9aa5625f02181eb8a
西村健人 <negai.m.nishimura@gmail.com>
2016-02-09 14:34:34 +0900

    delete 断捨離追加

--- db/schema.rb
+++ db/schema.rb
@@ -30,19 +30,6 @@ ActiveRecord::Schema.define(version: 20160205063213) do
     t.string   "name",         limit: 255
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  create_table "category_children", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.integer  "category_id", limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
-
   create_table "charas", force: :cascade do |t|
     t.string   "chara",      limit: 255
     t.string   "name",       limit: 255
