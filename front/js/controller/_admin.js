da10b79493652bf088bd23c3a7ca0ba50f5b9fe0
WadaHayato <hatoya.web.design@gmail.com>
2016-02-09 14:18:08 +0900

    add import buttons

--- front/js/controller/_admin.js
+++ front/js/controller/_admin.js
@@ -2,4 +2,5 @@ app.controller('adminController', function($scope, $http) {
     $scope.path = location.pathname;
     $scope.search = location.search;
     $scope.data = balloon_data.data;
+    console.log($scope.search);
 });
--- front/js/develop.js
+++ front/js/develop.js
@@ -76,6 +76,7 @@ app.controller('adminController', function($scope, $http) {
     $scope.path = location.pathname;
     $scope.search = location.search;
     $scope.data = balloon_data.data;
+    console.log($scope.search);
 });
 app.controller('balloonController', function($scope, $http) {
     $scope.data = balloon_data.data;
