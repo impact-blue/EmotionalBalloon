b6ab736801640e79bd49b671214f61a4ef767b15
西村健人 <negai.m.nishimura@gmail.com>
2016-02-16 18:56:48 +0900

    fix エラー文の日本語化

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -32,7 +32,6 @@ class CartsController < ApplicationController
     end
 
 
-
     if @order.save
       params[:data][:product_info].each_with_index do |product_info,i|
         @product = Product.find(product_info[:id])
@@ -41,7 +40,6 @@ class CartsController < ApplicationController
       end
       render json: {data:{result:"success"}}
     else
-
       render json: {data:
                           {result:"error",
                            "message":
