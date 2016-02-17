a189cf1dd17e1c6520ff07f316c8ca749a5f5bd8
西村健人 <negai.m.nishimura@gmail.com>
2016-02-17 16:01:47 +0900

    fix　バリデーションの追加

--- app/models/mail_content.rb
+++ app/models/mail_content.rb
@@ -1,2 +1,7 @@
 class MailContent < ActiveRecord::Base
+
+  varidates :text,    presence:  { message: 'は必須です' }
+
+  varidates :subject, presence:  { message: 'は必須です' },
+                      length:    {maximum: 400, message: 'は400文字以下にする必要があります' }
 end
