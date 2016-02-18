f9a7f09a076d898ddd3872f0fc26894b1dc253d7
西村健人 <negai.m.nishimura@gmail.com>
2016-02-18 10:40:35 +0900

    fix orderのバリデーションとカラムの並べ替え

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,25 +11,30 @@ class Order < ActiveRecord::Base
   accepts_nested_attributes_for :user
 
   validates :user_id,:price, presence: true,
-                                           numericality: {only_integer: true, message: 'は半角数字のみ入力できます。'}
-  #phoneのバリデーション
-  validates :phone, presence:  { message: 'は必須です' },
-                    length: { in: 10..15 , message: 'は10桁から15桁です。'},
-                    numericality: {only_integer: true, message: 'は半角数字のみ入力できます。'}
-
+                             numericality: {only_integer: true, message: 'は半角数字のみ入力できます。'}
   #postal_codeのバリデーション
-  validates :postal_code, presence: { message: 'は必須です' },
-                          numericality: {only_integer: true, :message => "は半角数字のみ入力できます" },
-                          length: {maximum: 9,wrong_length: 'の桁数が違います。'}
+  validates :postal_code,
+    presence: { message: 'は必須です' },
+    numericality: {only_integer: true, :message => "は半角数字のみ入力できます" },
+    length: {maximum: 9,wrong_length: 'の桁数が違います。'}
 
-  #cityのバリデーション
-  validates :city, presence: { message: 'は必須です' },
-                   exclusion: { in: %w({ }  . [ ] ) }
+  validates :city, :delivery_address, :payment_info, :order_status
+    presence: { message: 'は必須です' },
+    exclusion: { in: %w({ }  . [ ] ) }
 
-  #addressのバリデーション
-  validates :delivery_address, presence: { message: 'は必須です' },
-                      exclusion: { in: %w({ }  . [ ] ) }
-  validates :order_status, exclusion: { in: %w({ }  . [ ] ) }
+  #phoneのバリデーション
+  validates :phone,
+    presence:  { message: 'は必須です' },
+    length: { in: 10..15 , message: 'は10桁から15桁です。'},
+    numericality: {only_integer: true, message: 'は半角数字のみ入力できます。'}
+
+  validates :option,
+    exclusion: { in: %w({ }  . [ ] ) }
+
+  validates :scene_id,
+    length: { in: 1..2 },
+    numericality: {only_integer: true}
+
 
 
 end
--- db/migrate/20151126012425_create_orders.rb
+++ db/migrate/20151126012425_create_orders.rb
@@ -2,17 +2,17 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.integer :user_id
+      t.integer :price
       t.integer :postal_code
       t.string  :city
       t.string  :delivery_address
-      t.integer :phone
-      t.date    :order_date
       t.string  :payment_info
+      t.string  :order_status
+      t.integer :phone
       t.string  :option
       t.integer :scene_id
-      t.string  :order_status
-      t.integer :price
 
+      t.date    :order_date
       t.timestamps null: false
     end
   end
