cc53f2f36e797165af1c2871d5987af6916e14dd
WadaHayato <hatoya.web.design@gmail.com>
2016-02-18 16:50:25 +0900

    change stock to stocks

--- public/js/controller/_productCreate.js
+++ public/js/controller/_productCreate.js
@@ -3,7 +3,7 @@ app.controller('productCreateController', function($scope, $http) {
         id: balloon_data.data.detail_product.id,
         name: balloon_data.data.detail_product.name,
         price: balloon_data.data.detail_product.price,
-        stock: balloon_data.data.detail_product.stock,
+        stock: balloon_data.data.detail_product.stocks,
         images: [
             "http://uds.gnst.jp/rest/img/s3a4fr5t0000/s_00m9.jpg?t=1401720172",
             "http://uds.gnst.jp/rest/img/s3a4fr5t0000/s_00m9.jpg?t=1401720173",
