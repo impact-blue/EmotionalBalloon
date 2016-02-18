232a256d73796b5b78a6ffc24d7300d2a420ee03
西村健人 <negai.m.nishimura@gmail.com>
2016-02-18 15:11:48 +0900

    fix 商品購入のapiにトランザクションを追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,63 +12,66 @@ class CartsController < ApplicationController
 
 
   def api
-    @order = Order.new(order_params)
-    @order.price = 0
+    ActiveRecord::Base.transaction do
+      @order = Order.new(order_params)
+      @order.price = 0
 
-    params[:data][:product_info].each_with_index do |product_info,i|
-      #商品の合計値段計算
-      @order.price += Product.find(product_info[:id]).price
-      #購入商品登録
-      @order.order_product_infos.build
-      @order.order_product_infos[i].product_id = product_info[:id]
-    end
-    @order.postal_code      = params[:data][:destination_info][:postal_code]
-    @order.city             = params[:data][:destination_info][:prefectures]
-    @order.delivery_address = params[:data][:destination_info][:address]
-    @order.phone = params[:data][:destination_info][:phone]
-    #@order.date
-    @order.payment_info     = params[:data][:payment_info][:method]
-    @order.order_status     = "新着"
+      params[:data][:product_info].each_with_index do |product_info,i|
+        #商品の合計値段計算
+        @order.price += Product.find(product_info[:id]).price
+        #購入商品登録
+        @order.order_product_infos.build
+        @order.order_product_infos[i].product_id = product_info[:id]
+      end
+      @order.postal_code      = params[:data][:destination_info][:postal_code]
+      @order.city             = params[:data][:destination_info][:prefectures]
+      @order.delivery_address = params[:data][:destination_info][:address]
+      @order.phone = params[:data][:destination_info][:phone]
+      #@order.date
+      @order.payment_info     = params[:data][:payment_info][:method]
+      @order.order_status     = "新着"
 
-    @order.user = User.new(user_params)
-    @order.user.email       = params[:data][:buyer_info][:mail]
-    @order.user.phone       = params[:data][:buyer_info][:phone]
-    @order.user.postal_code = params[:data][:buyer_info][:postal_code]
-    @order.user.city        = params[:data][:buyer_info][:prefectures]
-    @order.user.address     = params[:data][:buyer_info][:address]
+      @order.user = User.new(user_params)
+      @order.user.email       = params[:data][:buyer_info][:mail]
+      @order.user.phone       = params[:data][:buyer_info][:phone]
+      @order.user.postal_code = params[:data][:buyer_info][:postal_code]
+      @order.user.city        = params[:data][:buyer_info][:prefectures]
+      @order.user.address     = params[:data][:buyer_info][:address]
 
-    #連名(User)
-    params[:data][:buyer_info][:name].each_with_index do |name,i|
-        @order.user.user_names.build
-        @order.user.user_names[i].user_family_name = name[:family_name]
-        @order.user.user_names[i].user_first_name  = name[:first_name]
-    end
-    #連名(Order)
-    params[:data][:destination_info][:name].each_with_index do |name,i|
-        @order.order_delivery_names.build
-        @order.order_delivery_names[i].order_family_name = name[:family_name]
-        @order.order_delivery_names[i].order_first_name  = name[:first_name]
-    end
-    #購入した商品のカウント
-    params[:data][:product_info].each_with_index do |product_info,i|
-        @product = Product.find(product_info[:id])
-        @product.count += params[:data][:product_info][i][:count]
-        @product.save!
-    end
-    #トランザクションで全て保存のみに対応にする。
-    if @order.save!
-      render json: {data:{result:"success"}}
-    else
-      render json: {data:
-                          {result:"error",
-                           "message":
-                            @order.errors.full_messages.each do |msg|
-                              [
-                                "msg",
-                              ]
-                            end
-                          }
-                   }
+      #連名(User)
+      params[:data][:buyer_info][:name].each_with_index do |name,i|
+          @order.user.user_names.build
+          @order.user.user_names[i].user_family_name = name[:family_name]
+          @order.user.user_names[i].user_first_name  = name[:first_name]
+      end
+      #連名(Order)
+      params[:data][:destination_info][:name].each_with_index do |name,i|
+          @order.order_delivery_names.build
+          @order.order_delivery_names[i].order_family_name = name[:family_name]
+          @order.order_delivery_names[i].order_first_name  = name[:first_name]
+      end
+      #購入した商品のカウント
+      params[:data][:product_info].each_with_index do |product_info,i|
+          @product = Product.find(product_info[:id])
+          @product.count += product_info[:count]
+          @product.save!
+      end
+
+      #トランザクションで全て保存のみに対応にする。
+      if @order.save!
+        render json: {data:{result:"success"}}
+      else
+        render json: {data:
+                            {result:"error",
+                             "message":
+                              @order.errors.full_messages.each do |msg|
+                                [
+                                  "msg",
+                                ]
+                              end
+                            }
+                     }
+      end
     end
   end
 
