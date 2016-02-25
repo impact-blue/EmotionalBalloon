a88206e3fa256d283f8a3a3f1e37a12c1b07a1f1
WadaHayato <hatoya.web.design@gmail.com>
2016-02-25 16:20:43 +0900

    add null to categoryContent if category create page

--- front/js/controller/_categoryEdit.js
+++ front/js/controller/_categoryEdit.js
@@ -1,10 +1,20 @@
 app.controller('categoryEditController', function($scope, $http) {
-    $scope.categoryContent = {
-        id: balloon_data.data.category_detail.id,
-        type: balloon_data.data.category_detail.type,
-        name_en: balloon_data.data.category_detail.name_en,
-        name_jp: balloon_data.data.category_detail.name_jp
-    };
+    if(location.pathname.split('/')[4] !== 'new') {
+        $scope.categoryContent = {
+            id: balloon_data.data.category_detail.id,
+            type: balloon_data.data.category_detail.type,
+            name_en: balloon_data.data.category_detail.name_en,
+            name_jp: balloon_data.data.category_detail.name_jp
+        };
+    } else {
+        $scope.categoryContent = {
+            id: null,
+            type: null,
+            name_en: null,
+            name_jp: null
+        };
+    }
+
 
     $scope.editCategory = function(key) {
         var sendData = {
