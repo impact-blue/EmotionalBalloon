5da2db1b15d1e9c1fd2bc222372dad9a4eb962be
WadaHayato <hatoya.web.design@gmail.com>
2016-02-25 12:00:14 +0900

    gulp build

--- public/js/application.js
+++ public/js/application.js
@@ -62,6 +62,9 @@ app.config(function($routeProvider, $locationProvider, $httpProvider) {
         .when('/admin/orders', {
             templateUrl: '/template/admin/order/index.html'
         })
+        .when('/admin/orders/:id/edit', {
+            templateUrl: '/template/admin/order/edit.html'
+        })
         .when('/admin/categories', {
             templateUrl: '/template/admin/category/index.html'
         })
@@ -411,6 +414,64 @@ app.controller('loginController', function($scope, $http) {
         if(e.which === 13) {
             $scope.loginPost();
         }
+    };
+});
+app.controller('orderEditController', function($scope, $http) {
+    $scope.orderContent = {
+        id: balloon_data.data.detail_order.id,
+        status: balloon_data.data.detail_order.status,
+        product_info: balloon_data.data.detail_order.product_info,
+        buyer_info: {
+            name: [
+                {
+                    family_name: balloon_data.data.detail_order.buyer_info.name[0].family_name,
+                    first_name: balloon_data.data.detail_order.buyer_info.name[0].first_name,
+                }
+            ],
+            postal_code: balloon_data.data.detail_order.buyer_info.postal_code,
+            prefectures: balloon_data.data.detail_order.buyer_info.prefectures,
+            address: balloon_data.data.detail_order.buyer_info.address,
+            phone: balloon_data.data.detail_order.buyer_info.phone,
+            mail: balloon_data.data.detail_order.buyer_info.mail
+        },
+        destination_info: {
+            name: [
+                {
+                    family_name: balloon_data.data.detail_order.destination_info.name[0].family_name,
+                    first_name: balloon_data.data.detail_order.destination_info.name[0].first_name,
+                }
+            ],
+            postal_code: balloon_data.data.detail_order.destination_info.postal_code,
+            prefectures: balloon_data.data.detail_order.destination_info.prefectures,
+            address: balloon_data.data.detail_order.destination_info.address,
+            date: balloon_data.data.detail_order.destination_info.date,
+            phone: balloon_data.data.detail_order.destination_info.phone,
+            classification: balloon_data.data.detail_order.destination_info.classification
+        },
+        payment_info: {
+            method: balloon_data.data.detail_order.payment_info.method,
+            token: balloon_data.data.detail_order.payment_info.token
+        },
+        comment: null
+    };
+
+    $scope.sendCart = function() {
+        var sendData = {
+            data: $scope.formContent
+        };
+        $http({
+            method: 'POST',
+            url: '/api/carts/comfirm',
+            data: sendData
+        }).success(function(data, status, headers, config) {
+            if(data.data.result === 'success') {
+                location.href = "/carts/thanks";
+            } else {
+                alert(data.data.message);
+            }
+        }).error(function(data, status, headers, config) {
+            alert(status);
+        });
     };
 });
 app.controller('productCreateController', function($scope, $http) {
