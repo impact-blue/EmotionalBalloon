ffc31acf5a4191beddaa0ec4a436c2ab179bec02
WadaHayato <hatoya.web.design@gmail.com>
2016-03-08 13:50:57 +0900

    add mail edit controller

--- front/js/application.js
+++ front/js/application.js
@@ -477,6 +477,31 @@ app.controller('loginController', function($scope, $http) {
         }
     };
 });
+app.controller('mailEditController', function($scope, $http) {
+    $scope.formModel = {
+        id: balloon_data.data.detail_mail.id,
+        subject: balloon_data.data.detail_mail.subject,
+        content: balloon_data.data.detail_mail.content
+    };
+    $scope.sendTemplate = function() {
+        var sendData = {
+            data: $scope.formModel
+        };
+        $http({
+            method: 'PATCH',
+            url: '/api/mails/edit',
+            data: sendData
+        }).success(function(data, status, headers, config) {
+            if(data.data.result === 'success') {
+                location.href = location.href;
+            } else {
+                alert(data.data.message);
+            }
+        }).error(function(data, status, headers, config) {
+            alert(status);
+        });
+    };
+});
 app.controller('orderEditController', function($scope, $http) {
     $scope.orderContent = {
         id: balloon_data.data.detail_order.id,
