34b015e718c37fd548fb59b74f34c9fef9c40ae1
WadaHayato <hatoya.web.design@gmail.com>
2016-03-08 16:15:44 +0900

    gulp build

---
+++ public/js/controller/_contactEdit.js
@@ -0,0 +1,26 @@
+app.controller('contactEditController', function($scope, $http) {
+    $scope.formOptions = {
+        status: ['未対応', '対応済み']
+    };
+    $scope.formModel = {
+        status: balloon_data.data.detail_contact.status
+    };
+    $scope.editContact = function() {
+        var sendData = {
+            data: $scope.formModel
+        };
+        $http({
+            method: 'PATCH',
+            url: '/api/contacts/edit',
+            data: sendData
+        }).success(function(data, status, headers, config) {
+            if(data.data.result === 'success') {
+                location.href = "/admin";
+            } else {
+                alert(data.data.message);
+            }
+        }).error(function(data, status, headers, config) {
+            alert(status);
+        });
+    };
+});
