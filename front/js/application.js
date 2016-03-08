bf360c2187d3e63f1efd2bfa8aa8c38047bbb4fd
WadaHayato <hatoya.web.design@gmail.com>
2016-03-08 11:08:51 +0900

    add sort filter

--- front/js/application.js
+++ front/js/application.js
@@ -596,11 +596,27 @@ app.controller('productListController', function($scope, $http, querySortService
     $scope.formOptions = {
         category: balloon_data.data.category_list
     };
-    $scope.formModel = {
-        category: null
-    };
+    $scope.formOptions.category.unshift({
+        id: null,
+        name_en: "all",
+        name_jp: "すべて",
+        type: ""
+    });
+    if(angular.isUndefined(querySortService.category)) {
+        $scope.formModel = {
+            category: null
+        };
+    } else {
+        $scope.formModel = {
+            category: Number(querySortService.category)
+        };
+    }
     $scope.findProduct = function(category) {
-        location.href = "/admin/products?status=" + querySortService['status'] + "&category=" + category;
+        if(category === null) {
+            location.href = "/admin/products?status=" + querySortService['status'];
+        } else {
+            location.href = "/admin/products?status=" + querySortService['status'] + "&category=" + category;
+        }
     };
 });
 app.controller('productShowController', function($scope, $http, getStorageService, saveStorageService) {
