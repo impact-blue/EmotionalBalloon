01f5a3565e5110c00f612355b5561bb00c60d275
WadaHayato <hatoya.web.design@gmail.com>
2016-03-08 17:49:54 +0900

    add export csv file

--- front/js/application.js
+++ front/js/application.js
@@ -100,6 +100,25 @@ app.controller('adminController', function($scope, $http) {
     $scope.split = $scope.path.split('/');
     $scope.search = location.search;
     $scope.data = balloon_data.data;
+
+    $scope.checkCSV = [];
+    $scope.tmpCSV = '';
+    $scope.$watch('checkCSV', function(value) {
+        $scope.tmpCSV = '';
+        angular.forEach($scope.checkCSV, function(val, index) {
+            if(val) {
+                $scope.tmpCSV = $scope.tmpCSV + '+' + balloon_data.data.search_products.product_list[index].id;
+            }
+        });
+        $scope.tmpCSV = $scope.tmpCSV.substr(1);
+    }, true);
+    $scope.exportCSV = function() {
+        if($scope.tmpCSV.length) {
+            location.href = '/admin/products.csv?id=' + $scope.tmpCSV;
+        } else {
+            location.href = '/admin/products.csv' + $scope.search;
+        }
+    };
 });
 app.controller('balloonController', function($scope, $http, querySortService, getStorageService) {
     $scope.data = balloon_data.data;
