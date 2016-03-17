5e8685465e0a50d94dcf3a3fb8ad395d10267937
西村健人 <negai.m.nishimura@gmail.com>
2016-03-17 18:38:41 +0900

    [fix]validates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
-  validates :subject,:content,:status, presence: { message: 'は必須です' },
+  validates :subject,:content, presence: { message: 'は必須です' },
                                        exclusion: { in: %w({ }  . [ ] ) }
 
 #お問い合わせの状態を日本語に対応
