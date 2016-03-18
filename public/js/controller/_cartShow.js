23a6f48cce9e5ccd99b8f3b3a314d7efca83d259
WadaHayato <hatoya.web.design@gmail.com>
2016-03-18 12:09:31 +0900

    :construction:

--- public/js/controller/_cartShow.js
+++ public/js/controller/_cartShow.js
@@ -1,12 +1,19 @@
-app.controller('cartShowController', function($scope, $http) {
-    $scope.cartItem = cartItem;
-    $scope.data = {};
-    angular.forEach($scope.cartItem, function(value, key){
+app.controller('cartShowController', function($scope, $http, localStorageService) {
+    $scope.cartItem = [];
+    $scope.sumPrice = 0;
+    angular.forEach(localStorageService.load().cart, function(value, key){
         $http({
             method: 'GET',
-            url: '/api/products/detail.json'
+            url: '/api/products/detail.json?id=' + value
         }).success(function(data, status, headers, config) {
-            angular.merge($scope.data, data.data);
+            if(data.result === 'success') {
+                $scope.cartItem.push(data.data.product_detail);
+            } else {
+                console.log(data.message);
+            }
+            $scope.sumPrice += data.data.product_detail.price;
+        }).error(function(data, status, headers, config) {
+            alert(status);
         });
     });
 });
--- public/js/controller/_productList.js
+++ public/js/controller/_productList.js
@@ -1,27 +1,26 @@
 app.controller('productListController', function($scope, $http, querySortService) {
     $scope.formOptions = {
-        category: balloon_data.data.category_list
+        category: balloon_data.data.category_list,
+        per_page: [
+            20,
+            50,
+            100
+        ]
     };
     $scope.formOptions.category.unshift({
-        id: null,
+        id: 0,
         name_en: "all",
         name_jp: "すべて",
         type: ""
     });
-    if(angular.isUndefined(querySortService.category)) {
-        $scope.formModel = {
-            category: null
-        };
-    } else {
-        $scope.formModel = {
-            category: Number(querySortService.category)
-        };
-    }
-    $scope.findProduct = function(category) {
-        if(category === null) {
-            location.href = "/admin/products?status=" + querySortService['status'];
-        } else {
-            location.href = "/admin/products?status=" + querySortService['status'] + "&category=" + category;
-        }
+    $scope.formModel = {
+        category: Number(querySortService.category),
+        per_page: Number(querySortService.per_page)
     };
+    $scope.$watch('formModel', function(value) {
+        console.log(value.category, querySortService.category);
+        if(value.category !== Number(querySortService.category) || value.per_page !== Number(querySortService.per_page)) {
+            location.href = "/admin/products?status=" + querySortService['status'] + "&category=" + value.category + "&per_page=" + value.per_page;
+        }
+    }, true);
 });
